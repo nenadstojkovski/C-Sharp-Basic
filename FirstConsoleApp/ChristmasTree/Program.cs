7af4ec3d1042cffff9fd547eefe2addd2feb228d
nenadstojkovski <nenad.stojkovski93@gmail.com>
2019-02-27 12:44:05 +0100

    demo

---
+++ FirstConsoleApp/ChristmasTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("     * ");
+            Console.WriteLine("    *** ");
+            Console.WriteLine("   ***** ");
+            Console.WriteLine("  ******* ");
+            Console.WriteLine(" ********* ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("    | | ");
+            Console.WriteLine("    | | ");
+            Console.ForegroundColor = oldColor;
+            Console.ReadLine();
+
+
+        }
+    }
+}
